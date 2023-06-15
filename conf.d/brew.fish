64735e85ee7a76589045e51625b98d00bd2f46e5
vanyauhalin <vanyauhalin@gmail.com>
2023-06-15 21:03:49 +0400

    Rename brew to homebrew

--- conf.d/brew.fish
+++
@@ -1,3 +0,0 @@
-function __brew_set_environment
-	eval (command brew shellenv)
-end
--- config.fish
+++ config.fish
@@ -6,9 +6,9 @@ __xdg_backport_data_home
 
 set -gx HISTFILE $XDG_STATE_HOME/shell/history
 
-__brew_set_environment
 __docker_set_environment
 __fish_set_environment
+__homebrew_set_environment
 __javascript_set_environment
 __less_set_environment
 __python_set_environment
