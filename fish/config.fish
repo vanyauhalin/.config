4458ec4214da8d0a17e569b3ee55095ecb9d591e
vanyauhalin <vanyauhalin@gmail.com>
2023-07-24 22:20:29 +0400

    Change the short flag to a long one

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 fish_add_path /sbin /usr/local/bin /usr/sbin
 
 set --global fish_greeting
-set --export --global SHELL (command -v fish)
+set --export --global SHELL (command --search fish)
 
 __fisher_autoload
