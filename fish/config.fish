e781be4acd348f2653a9367aeeb50778dcfda857
vanyauhalin <vanyauhalin@gmail.com>
2023-07-24 09:05:32 +0400

    Delete unnecessary shell history env variable

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,5 @@ fish_add_path /sbin /usr/local/bin /usr/sbin
 
 set --global fish_greeting
 set --export --global SHELL (command -v fish)
-set --export --global HISTFILE $XDG_STATE_HOME/shell/history
 
 __fisher_autoload
