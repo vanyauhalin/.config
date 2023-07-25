87f35578c9d5caa2a9f2460b5789b80ba303b06a
vanyauhalin <vanyauhalin@gmail.com>
2023-07-25 19:45:20 +0400

    Add brew export command

--- homebrew/completions/brew.fish
+++ homebrew/completions/brew.fish
@@ -3,5 +3,10 @@ __fisher_find_completion brew
 complete \
 	--command brew \
 	--condition __fish_use_subcommand \
+	--arguments export \
+	--description "Export lists of manually installed formulae and casks"
+complete \
+	--command brew \
+	--condition __fish_use_subcommand \
 	--arguments yolo \
 	--description "Run update, upgrade, autoremove and cleanup"
--- homebrew/functions/brew.fish
+++ homebrew/functions/brew.fish
@@ -4,16 +4,56 @@ function brew
 		command brew $argv
 		echo ""
 		echo "Additional subcommands:"
+		echo "  export              Export lists of manually installed formulae and casks."
 		echo "  yolo                Run update, upgrade, autoremove and cleanup."
 		if test -z "$argv"
 			return 1
 		end
+
+	case autoremove cleanup "install*" "tap*" "uninstall*" "untap*" update "upgrade*"
+		command brew $argv
+		brew export
+
+	case export
+		set --local directory $XDG_CONFIG_HOME/homebrew
+
+		set --local casks_file $directory/casks
+		set --local casks (command brew list --cask --version)
+		set --local casks_content (string join "\n" $casks)
+		echo -e $casks_content > $casks_file
+
+		set --local requested (command brew leaves --installed-on-request)
+		set --local formulae (command brew list --formula --version)
+		set --local export
+
+		for item in $requested
+			set --local exported
+
+			for formula in $formulae
+				set --local matched (string match "$item*" $formula)
+				if test $matched
+					set exported $matched
+					break
+				end
+			end
+
+			if test $exported
+				set --append export $exported
+			else
+				set --append export $item
+			end
+		end
+
+		set --local formulae_file $directory/formulae
+		set --local formulae_content (string join "\n" $export)
+		echo -e $formulae_content > $formulae_file
 
 	case yolo
 		command brew update
 		command brew upgrade
 		command brew autoremove
 		command brew cleanup
+		brew export
 
 	case \*
 		command brew $argv
