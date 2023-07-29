cfc02566f4604faca59d75a4b48b2740040f4292
vanyauhalin <vanyauhalin@gmail.com>
2023-07-29 10:33:12 +0400

    Update ruby env variables

--- ruby/conf.d/ruby.fish
+++ ruby/conf.d/ruby.fish
@@ -1,10 +1,8 @@
-# # https://github.com/rbenv/ruby-build/blob/v20230512/README.md#custom-build-configuration
-# set -gx RUBY_BUILD_BUILD_PATH $XDG_RUNTIME_DIR/ruby
-# set -gx RUBY_BUILD_CACHE_PATH $XDG_CACHE_HOME/ruby
-
 # https://github.com/ruby/ruby/blob/v3_3_0_preview1/doc/contributing/building_ruby.md#dependencies
-set -gx RUBY_CONFIGURE_OPTS \
-	--with-libyaml-dir=(command brew --prefix libyaml) \
-	--with-openssl-dir=(command brew --prefix openssl@3) \
-	--with-readline-dir=(command brew --prefix readline) \
-	--with-zlib-dir=(command brew --prefix zlib)
+set --export --global RUBY_CONFIGURE_OPTS \
+	--with-libyaml-dir=(brew --prefix libyaml) \
+	--with-openssl-dir=(brew --prefix openssl@3) \
+	--with-readline-dir=(brew --prefix readline) \
+	--with-zlib-dir=(brew --prefix zlib)
+
+set --export --global RUBY_YJIT_ENABLE 1
